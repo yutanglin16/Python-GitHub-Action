c9c9736c33c1a8b5989a8bd286424bc0bdd9585c
yiliaomsft <yili@microsoft.com>
2019-05-01 15:29:13 -0700

    first commit

---
+++ mydjangoproject/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+
+urlpatterns = [
+    path("", include('welcome.urls')),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += staticfiles_urlpatterns()
