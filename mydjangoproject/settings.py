c233f2888f864d67ed309aa4584cd6eb59e1beee
Jason Freeberg <jr.freeberg@comcast.net>
2019-09-25 17:00:42 -0700

    new build w/ DB

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 # Add your database connection strings here. Comment out the SQLite DB below.
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '<database name>',
-#         'USER': '<database user name>',
-#         'PASSWORD': 'database password',
-#         'HOST': 'database host',
-#      }
-# }
-
-# Database for local testing/development. Comment this out for deployment
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pollsAppDatabase',
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+     }
 }
+
+# Database for local testing/development. Comment this out for deployment
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#     }
+# }
 
 
 
